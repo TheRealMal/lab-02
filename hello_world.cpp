23c33ce2cbcf2f5e1548a767e176a701dea60d8d
therealmal <44868259+TheRealMal@users.noreply.github.com>
2021-03-10 23:27:49 +0300

    Changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,11 @@
 
 using namespace std;
 
-int main(){
-    string name;
-    cin >> name;
-    cout << "Hello world from " << name;
-    return 0;
-} 
+int
+main()
+{
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name;
+  return 0;
+}
